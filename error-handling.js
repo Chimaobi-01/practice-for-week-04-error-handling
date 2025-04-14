33a1ceebfed576cbf27acb55f7562e790d22f850
Chinaka_Chimaobi <80992410+Chimaobi-01@users.noreply.github.com>
2025-04-14 16:14:57 +0000

    handles all errors

--- error-handling.js
+++ error-handling.js
@@ -6,16 +6,30 @@ function sum(array) {
   }
   return sum;
 }
-
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.log(error.message)
+  }
+}
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+try {
+  sayName("Alex")
+  sayName(1);
+} catch (e) {
+  console.log(e.message)
+}
 // Your code here
-
+function sayName(name) {
+  if (typeof name !== "string") {
+    throw Error("Invalid name! Must be a string!")
+  }
+  console.log(name)
+}
 // 3.
 function greet(greeting) {
   if (!greeting) {
@@ -23,4 +37,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet()
+} catch (error) {
+  console.log("Hello World!")
 }
